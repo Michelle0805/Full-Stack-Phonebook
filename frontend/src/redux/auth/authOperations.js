c3ce1919c96849e70d8b54cf8599a02968c6bb96
Michelle0805 <vasudaria85@gmail.com>
2024-06-20 13:48:56 +0800

    modify baseURL

--- frontend/src/redux/auth/authOperations.js
+++ frontend/src/redux/auth/authOperations.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'http://localhost:8000/api';
+axios.defaults.baseURL = 'https://full-stack-phonebook-63zc.onrender.com/api';
 
 // Utility to add JWT
 const setAuthHeader = token => {
